359f62b59649a3bab9dc62d339cd7cb5d28d759d
ahenteti <ahmad.henteti@gmail.com>
2020-07-15 21:48:21 +0200

    add env variable to define server port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get('/', function (req: any, res: any) {
+  console.log('enter get / method');
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Server is running on port ' + port);
 });
