f310bb07564cba5b258567d3b313b1de8c9aa85c
ahenteti <ahmad.henteti@gmail.com>
2020-07-15 22:45:09 +0200

    add sass npm utility to style index.html file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express = require('express');
 const app: express.Application = express();
 const port = process.env.PORT || 4000;
 
+app.use(express.static(__dirname));
+
 app.get('/', function (req, res) {
   console.log("enter get('/') method");
   res.sendFile(__dirname + '/index.html');
